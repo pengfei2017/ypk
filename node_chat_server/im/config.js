5eaa5d916eecb33835806a3bc447307535441b83
贺鹏飞 <hepengfei@chuangjiangx.com>
2017-12-18 18:10:11 +0800

    添加nodejs聊天模块

---
+++ node_chat_server/im/config.js
@@ -0,0 +1,19 @@
+/**
+ * xingsu
+ */
+var config = {};//数据库帐号设置
+
+//config['host'] = '180.76.141.156';//数据库地址
+config['host'] = 'ypk-database';//数据库地址
+config['port'] = '3306';//数据库端口
+config['user'] = 'root';//数据库用户名
+config['password'] = 'hpf';//数据库密码
+config['database'] = 'ypk';//mysql数据库名
+config['tablepre'] = '';//表前缀
+config['insecureAuth'] = true;//兼容低版本
+config['debug'] = true;//默认false
+
+//exports.hostname = '192.168.1.104';//把网址修改为你安装商城的域名，不要带http://及/
+exports.hostname = '127.0.0.1';//把网址修改为你安装商城的域名，不要带http://及/
+exports.port = 8096;//服务器所用端口号,默认8090
+exports.config = config;
