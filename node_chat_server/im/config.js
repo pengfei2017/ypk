454cecd0af03c319b9c8cf43d18205ae05977d1a
贺鹏飞 <hepengfei@chuangjiangx.com>
2017-12-19 15:32:58 +0800

    添加nodejs聊天模块

--- node_chat_server/im/config.js
+++ node_chat_server/im/config.js
@@ -14,6 +14,6 @@ config['insecureAuth'] = true;//兼容低版本
 config['debug'] = true;//默认false
 
 //exports.hostname = '192.168.1.104';//把网址修改为你安装商城的域名，不要带http://及/
-exports.hostname = 'node-chat-server';//把网址修改为你安装商城的域名，不要带http://及/
+exports.hostname = 'localhost';//把网址修改为你安装商城的域名，不要带http://及/
 exports.port = 8096;//服务器所用端口号,默认8090
 exports.config = config;
